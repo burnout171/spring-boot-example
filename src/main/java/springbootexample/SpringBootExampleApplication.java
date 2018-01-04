f52e20fea393cd99e0d1409061a4532c8a617e91
Pavel Likin <pavellikin@gmail.com>
2018-01-04 23:14:59 +0300

    Mongo configuration will be applied only for mongo-aggregation profile.

--- src/main/java/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/springbootexample/SpringBootExampleApplication.java
@@ -2,8 +2,12 @@ package springbootexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "springbootexample")
+@SpringBootApplication(
+		scanBasePackages = "springbootexample",
+		exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class SpringBootExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExampleApplication.class, args);
--- src/main/java/springbootexample/configuration/MongoDbConfiguration.java
+++ src/main/java/springbootexample/configuration/MongoDbConfiguration.java
@@ -1,9 +1,13 @@
 package springbootexample.configuration;
 
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("mongo-aggregation")
+@Import(value = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MongoDbConfiguration {
 }
